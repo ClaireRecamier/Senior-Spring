4315a288ea2bf9d80822cd408e1b6540cb73db4a
Claire Recamier <recamierseven@gmail.com>
2018-05-14 14:28:45 -0400

    added methods to js file

--- main.js
+++ main.js
@@ -2,10 +2,66 @@ class Post (){
   constructor() {}
 
 
-  saveDraft(){}
+  savePost(){
+      var postId = chance.guid();
+      // var postDesc = document.getElementById('issueDescInput').value;
+      // var issueSeverity = document.getElementById('issueSeverityInput').value;
+      // var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+      // var issueStatus = 'Open';
+      var post = {
+        id: postId,
+        // description: postDesc,
+        // severity: postSeverity,
+        // assignedTo: postAssignedTo,
+        // status: postStatus
+      }
+
+      if (localStorage.getItem('posts') === null) {
+        var posts = [];
+        posts.push(post);
+        localStorage.setItem('posts', JSON.stringify(posts));
+      } else {
+        var posts = JSON.parse(localStorage.getItem('posts'));
+        posts.push(posts);
+        localStorage.setItem('posts', JSON.stringify(posts));
+      }
+
+      document.getElementById('postsInputForm').reset();
+
+      fetchPosts();
+
+      e.preventDefault();
+
+  }
+
   publishPost(){}
-  fetchPost(){}
-  deletePost(){}
+
+  fetchPosts(){
+    var posts = JSON.parse(localStorage.getItem('posts'));
+    var postsList = document.getElementById('postsList');
+
+    for (var i = 0; i < posts.length; i++) {
+      var id = posts[i].id;
+      // var desc = posts[i].description;
+      // var severity = posts[i].severity;
+      // var assignedTo = posts[i].assignedTo;
+      // var status = posts[i].status;
+  }
+
+  deletePost(){
+
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for(var i = 0; i < issues.length; i++) {
+      if (issues[i].id == id) {
+        issues.splice(i, 1);
+      }
+    }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchPosts();
+  }
 
 
 
